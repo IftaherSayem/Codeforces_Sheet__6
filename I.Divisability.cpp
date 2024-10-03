479d015adf4a790fa764df7c40aabcf97dbb1ef7
IftaherSayem <iftahersayem205@gmail.com>
2024-10-03 08:40:13 +0600

    Create I.Divisability.cpp

---
+++ I.Divisability.cpp
@@ -0,0 +1,47 @@
+/*
+========================================
++          IFTAHER HOSSEN SAYEM        +
++            IIUC,Dept. of CSE         +
+========================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+void test()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+int finding(char ch)
+{
+    return ch-'A'+1;
+}
+int posi(int h)
+{
+    return h+'a'-1;
+}
+bool deco(pair <int,int>&a,pair<int,int>&b)
+{
+    return a.second>b.second;
+}
+ll sum(ll a)
+{
+   return a*(a+1) / 2;
+}
+int main()
+{
+    //test();
+    ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+    //ll n;
+    //cin >> n;
+    //while(n--)
+    //{
+    ll a,b,c,d;
+    cin >> a >> b >> c;
+    ll maxi=max(a,b);
+    ll mini=min(a,b);
+    ll ans=(sum(maxi/c)*c)-(sum((mini-1)/c)*c);
+    cout << ans << endl;
+    //}
+}
